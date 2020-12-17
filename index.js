b18bee8862459908d22fdb411a5c4c8a2734fa07
scottsam <samuel_owulezi@yahoo.com>
2020-12-17 23:51:17 +0100

    second commit

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.use((req, res, next) => {
 const port = process.env.PORT || 8080;
 
 mongoose
-  .connect(process.env.MONGO_URI, {
+  .connect(process.env.MONGO_URI || "mongodb://localhost:27017/mernDb", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
